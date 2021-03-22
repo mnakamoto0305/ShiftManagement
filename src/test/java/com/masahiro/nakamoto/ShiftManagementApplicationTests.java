1a7b65784cf659c49bdb1657602a1b4395351b9b
mnakamoto0305 <m.nakamoto0305@gmail.com>
2021-03-22 18:09:50 +0900

    first commit

---
+++ src/test/java/com/masahiro/nakamoto/ShiftManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masahiro.nakamoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
