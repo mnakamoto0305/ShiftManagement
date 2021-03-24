7ccf9347183d103ed5494bd1c817bf4a2d79a84c
mnakamoto0305 <m.nakamoto0305@gmail.com>
2021-03-24 22:29:24 +0900

    追加：休み希望登録時のバリデーション設定

--- src/main/java/com/masahiro/nakamoto/domain/RegisterHolidayForm.java
+++ src/main/java/com/masahiro/nakamoto/domain/RegisterHolidayForm.java
@@ -3,11 +3,14 @@ package com.masahiro.nakamoto.domain;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class RegisterHolidayForm {
 
+	@Size(min=87,max=87,message="休み希望は8日分入力してください")
 	private String holiday;
 
 	private List<LocalDate> holidayList;
