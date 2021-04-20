ba0f93b337706e0dbd7da6c9b80ceff8113a0878
mnakamoto0305 <m.nakamoto0305@gmail.com>
2021-04-20 15:02:00 +0900

    修正：リファクタリング内容に基づいてテストケースを変更

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO drivers VALUES('koto@sample.com', 2, '佐藤', '太郎', 1, '1990-01-01', '1000004', '東京都千代田区大手町1-1-1', '08012345678', '2020-01-01', 1, 1, 20000, 30000);
 INSERT INTO drivers VALUES('koto2@sample.com', 2, '山田', '太郎', 1, '1990-01-01', '1000004', '東京都千代田区大手町1-1-1', '08012345678', '2020-01-01', 1, 2, 20000, 30000);
-INSERT INTO drivers VALUES('edogawa2@sample.com', 2, '佐藤', '太郎', 1, '1990-01-01', '1000004', '東京都千代田区大手町1-1-1', '08012345678', '2020-01-01', 2, 2, 20000, 30000);
+INSERT INTO drivers VALUES('tyuo@sample.com', 2, '佐藤', '太郎', 1, '1990-01-01', '1000004', '東京都千代田区大手町1-1-1', '08012345678', '2020-01-01', 4, 1, 20000, 30000);
 
 
 INSERT INTO users VALUES('koto@sample.com', '$2a$10$zjHq1xmqY3/cgKSpK7dywOSYFsthdmUrBhOZ8YjzTyrJxSqfEBJYG', 'USER', 2, '2021-04-12 12:29:41');
 INSERT INTO users VALUES('koto2@sample.com', '$2a$10$zjHq1xmqY3/cgKSpK7dywOSYFsthdmUrBhOZ8YjzTyrJxSqfEBJYG', 'USER', 2, '2021-04-12 12:29:41');
-INSERT INTO users VALUES('edogawa2@sample.com', '$2a$10$zjHq1xmqY3/cgKSpK7dywOSYFsthdmUrBhOZ8YjzTyrJxSqfEBJYG', 'USER', 2, '2021-04-12 12:29:41');
+INSERT INTO users VALUES('tyuo@sample.com', '$2a$10$zjHq1xmqY3/cgKSpK7dywOSYFsthdmUrBhOZ8YjzTyrJxSqfEBJYG', 'USER', 2, '2021-04-12 12:29:41');
 
 INSERT INTO areas VALUES(1, '江東', 5, 7);
 INSERT INTO areas VALUES(2, '江戸川', 6, 9);
